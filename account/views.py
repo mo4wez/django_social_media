7189e72409b6ad8394418c9ffc3102b0f8eef6b4
moawezz <moawez.de@gmail.com>
2024-03-31 17:50:15 +0330

    add register form, template, view

--- account/views.py
+++ account/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 
+from .forms import UserRegisterationForm
+
 class RegisterView(View):
     def get(self, request):
-        return render(request, 'account/register.html')
+        form = UserRegisterationForm()
+        return render(request, 'account/register.html', {'form': form})
 
     def post(self, request):
         pass
