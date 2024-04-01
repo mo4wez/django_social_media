4cf43d7973aa7fba87f0ab5d5238407ea107e65b
moawezz <moawez.de@gmail.com>
2024-04-01 21:54:37 +0330

    add post method for register a user

--- account/views.py
+++ account/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 from .forms import UserRegisterationForm
 
@@ -9,4 +11,19 @@ class RegisterView(View):
         return render(request, 'account/register.html', {'form': form})
 
     def post(self, request):
-        pass
+        form = UserRegisterationForm(request.POST)
+        
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(
+                username=cd['username'],
+                email=cd['email'],
+                password=cd['password'],
+                )
+            messages.success(
+                request,
+                'You registered successfully.',
+                'success',
+                )
+            return redirect('home:home_page')            
+
