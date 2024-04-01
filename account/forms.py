84ae7a8db2fe8940c1f49da3575a2adf57ab253c
moawezz <moawez.de@gmail.com>
2024-04-01 23:04:08 +0330

    override clean method to check passwords match

--- account/forms.py
+++ account/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
@@ -6,7 +7,8 @@ from django.contrib.auth.models import User
 class UserRegisterationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control',}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control',}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control',}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control',}))
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class':'form-control',}))
 
     def clean_username(self):
         username = self.cleaned_data['username']
@@ -17,8 +19,18 @@ class UserRegisterationForm(forms.Form):
     def clean_email(self):
         email = self.cleaned_data['email']
         if User.objects.filter(email=email).exists():
-            raise ValidationError('Email exists, Try another one.', 'invalid')
+            raise ValidationError('Email exists, Try another one.')
         return email
+    
+    def clean(self):
+        # cleaned data
+        cd = super().clean()
+
+        password1 = cd.get('password1')
+        password2 = cd.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise ValidationError('Passwords does not match.')
+
 
 class UserLoginForm(forms.Form):
     usename = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
