2e7e9ccd4ec01b5de5fcdb52bef93fa5d9eef34a
moawezz <moawez.de@gmail.com>
2024-04-02 21:35:28 +0330

    Customizing authentication backends - login by email

--- config/settings.py
+++ config/settings.py
@@ -132,4 +132,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "account.authenticate.EmainBackend",
+    ]
+
 LOGIN_URL = '/account/login/'
