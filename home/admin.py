f0b73b7977c8e6efd4f9d751588beb0a83340030
moawezz <moawez.de@gmail.com>
2024-04-03 16:53:02 +0330

    customize PostAdmin

--- home/admin.py
+++ home/admin.py
@@ -4,8 +4,8 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['body', 'user', 'datetime_modified', 'datetime_created',]
+    list_display = ['title', 'user', 'datetime_modified', 'datetime_created',]
     search_fields = ['slug',]
     list_filter = ['datetime_modified',]
-    prepopulated_fields = {'slug':('body',),}
+    prepopulated_fields = {'slug':('title',),}
     raw_id_fields = ['user',]
