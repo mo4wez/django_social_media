6220ef165a4b47a799959bbc5ff4eaf50ed7daf4
moawezz <moawez.de@gmail.com>
2024-04-03 16:55:09 +0330

    add PostDetailView

--- home/views.py
+++ home/views.py
@@ -8,3 +8,11 @@ class HomePageView(View):
     def get(self, request):
         posts = Post.objects.all()
         return render(request, 'home/index.html', {'posts': posts})
+
+
+class PostDetailView(View):
+
+    def get(self, request, post_id, slug):
+        post = Post.objects.get(pk=post_id, slug=slug)
+
+        return render(request, 'home/post_detail.html', {'post': post})
