ef787131fdf1fc66afd776e6cfab46194748169f
moawezz <moawez.de@gmail.com>
2024-04-03 15:58:32 +0330

    add Post model

--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
     "account.authenticate.EmainBackend",
-    ]
+]
 
 LOGIN_URL = '/account/login/'
--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    body = models.TextField()
+    slug = models.SlugField()
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body[:10]
