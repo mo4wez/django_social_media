ac627ba552345f8ef22480542341a80139124422
moawezz <moawez.de@gmail.com>
2024-04-03 16:31:58 +0330

    add posts in homeview

--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 
-class HomePageView(View):
-    def get(self, request):
-        return render(request, 'home/index.html')
+from .models import Post
 
+class HomePageView(View):
+
+    def get(self, request):
+        posts = Post.objects.all()
+        return render(request, 'home/index.html', {'posts': posts})
