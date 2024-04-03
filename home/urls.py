a9267e2a807ccdac6e2ea53c1eb81330ebaaee57
moawezz <moawez.de@gmail.com>
2024-04-03 16:54:51 +0330

    add post detail url

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home_page'),
+    path('posts/<int:post_id>/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
