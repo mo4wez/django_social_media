39db75ffd3b13cf720548d33abacf4944802cf83
moawezz <moawez.de@gmail.com>
2024-04-04 13:32:17 +0330

    add post delete view

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post
 
@@ -16,3 +18,29 @@ class PostDetailView(View):
         post = Post.objects.get(pk=post_id, slug=slug)
 
         return render(request, 'home/post_detail.html', {'post': post})
+
+
+class PostDeleteView(LoginRequiredMixin, View):
+
+    def get(self, request, post_id):
+        return render(request, 'home/post_delete.html')
+    
+    def post(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        if post.user.id == request.user.id:
+            post.delete()
+            messages.success(
+                request, 
+                'Your was post deleted.',
+                'success'
+            )
+            return redirect('home:home_page')
+        
+        messages.error(
+            request,
+            'You can not delete this post.',
+            'danger'
+        )
+        
+        return render(request, 'home/post_delete.html')
+        
