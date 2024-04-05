8fbea927d78845ec08bf2905dc8eb8749adc6af2
moawezz <moawez.de@gmail.com>
2024-04-05 21:41:32 +0330

    combine create and update form together

--- home/forms.py
+++ home/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 from .models import Post
 
-class PostUpdateForm(forms.ModelForm):
+class PostCreateAndUpdateForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'body',]
