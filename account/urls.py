2927154d639157b8d553deabcd8d27e40ee95c2b
moawezz <moawez.de@gmail.com>
2024-04-06 23:33:18 +0330

    add password reset url

--- account/urls.py
+++ account/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login_user'),
     path('logout/', views.UserLogOutView.as_view(), name='logout_user'),
     path('profile/<int:user_id>/', views.UserProfileView.as_view(), name='user_profile'),
+    path('reset/', views.UserPasswordResetView.as_view(), name='password_reset'),
 ]
