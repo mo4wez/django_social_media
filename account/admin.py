78b86332600028b0c655e74c3c9c7693064cd03b
moawezz <moawez.de@gmail.com>
2024-04-08 12:26:29 +0330

    add raw_id_fields to RelationAdmin

--- account/admin.py
+++ account/admin.py
@@ -6,3 +6,4 @@ from .models import Relation
 @admin.register(Relation)
 class RelationAdmin(admin.ModelAdmin):
     list_display = ['from_user', 'to_user', 'datetime_created',]
+    raw_id_fields = ['from_user', 'to_user',]
