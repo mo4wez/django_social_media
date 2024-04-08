4b46d71075a053bce9edb393c195a3eda65f1cd7
moawezz <moawez.de@gmail.com>
2024-04-08 12:26:53 +0330

    add Follow and Unfollow urls

--- account/urls.py
+++ account/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('reset/done/', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
     path('confirm/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('confirm/complete/', views.UserPasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('follow/<int:user_id>/', views.UserFollowView.as_view(), name='follow_user'),
+    path('unfollow/<int:user_id>/', views.UserUnfollowView.as_view(), name='unfollow_user'),
 ]
